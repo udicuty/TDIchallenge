8c0f86ccad83118eb8cf125ae3d0e906636bfe86
udicuty <udi.rubin@gmail.com>
2016-01-31 22:25:36 -0500

    added linear fit

--- TDIq3.R
+++ TDIq3.R
@@ -2,7 +2,7 @@ require(data.table)
 require(dplyr)
 require(lubridate)
 require(ggplot2)
-require(gtools)
+#require(gtools)
 
 
 #Reading central park temperatures and filtering for dates that intersect with NYPD accident records
@@ -15,12 +15,12 @@ NYPD<-fread("Data/NYPD_Motor_Vehicle_Collisions.csv",data.table=FALSE)
 colnames(NYPD)<-make.names(colnames(NYPD))
 NYPD$DATE<-mdy(NYPD$DATE)
 M<-filter(NYPD,BOROUGH=="MANHATTAN") %>% arrange(DATE)
-#M<-filter(NYPD,NUMBER.OF.PERSONS.INJURED<100) %>% arrange(DATE) #Removing outliers
+M<-filter(NYPD,NUMBER.OF.PERSONS.INJURED<100) %>% arrange(DATE) #Removing outliers
 #M$MONTH<-substring(M$DATE,1,7)
 
 #Summarizing information from NYPD file, "rat" stands for injured/incidents ratio
-i<-group_by(M,DATE) %>% summarize(incidents=n(),injured=sum(NUMBER.OF.PERSONS.INJURED),ped_injured=sum(NUMBER.OF.PEDESTRIANS.INJURED),death=sum(NUMBER.OF.PERSONS.KILLED),rat=injured/incidents)
-
+i<-group_by(M,DATE) %>% summarize(ped_injured=sum(NUMBER.OF.PEDESTRIANS.INJURED),non_ped_injured=sum(NUMBER.OF.PERSONS.INJURED)-ped_injured)
+head(i)
 
 #Considering only dates to which we have information in both databases
 common_dates<-intersect(w$DATE,i$DATE)
@@ -31,23 +31,32 @@ M<-filter(M,DATE %in% common_dates)
 
 #Merging NYPD with weather data
 d<-cbind(i,PRCP=w$PRCP)
+head(d)
+d$DATE<-as.character(d$DATE)
+
 
 #Calculating corellation and fitting linear model between injuries and percipitation levels
-cor(d$injured,d$PRCP)
-x<-coef(lm(d$injured~d$PRCP))
-xintercept<-as.numeric(x[1])
-xslope<-as.numeric(x[2])
-xintercept
-xslope
+cor(d$non_ped_injured,d$PRCP)
+nonped<-coef(lm(d$non_ped_injured~d$PRCP))
+nonped_intercept<-as.numeric(nonped[1])
+nonped_slope<-as.numeric(nonped[2])
+nonped_intercept
+nonped_slope
 
 cor(d$ped_injured,d$PRCP)
-y<-coef(lm(d$ped_injured~d$PRCP))
-yintercept<-as.numeric(y[1])
-yslope<-as.numeric(y[2])
-yslope
+ped<-coef(lm(d$ped_injured~d$PRCP))
+ped_intercept<-as.numeric(ped[1])
+ped_slope<-as.numeric(ped[2])
+ped_slope
 #d$cyclist<-as.factor(ifelse(d$cyclist>0,1,0))
-ggplot(d,aes(PRCP,ped_injured)) + geom_point() + geom_abline(intercept= xintercept,slope=xslope,col="Red") + geom_abline(intercept= yintercept,slope=yslope,col="Red")
 
+
+dd<-melt(d,measure.vars=c("ped_injured","non_ped_injured"))
+head(dd)
+
+ggplot(dd,aes(PRCP,value,col=variable)) + geom_point()
+
+ggplot(dd,aes(PRCP,value,col=variable)) + geom_point() + geom_abline(intercept= nonped_intercept,slope=nonped_slope,col="cyan",size=1.5) + geom_abline(intercept= ped_intercept,slope=ped_slope,col="Orange",size=1.5)
 
 
 
@@ -63,13 +72,36 @@ x[x==""]<-"UNKNOWN"
 
 
 
-by_rain<-group_by(M,rainy_days) %>% summarize(incidents=n(),injured=sum(NUMBER.OF.PERSONS.INJURED),
-            ped_injured=sum(NUMBER.OF.PEDESTRIANS.INJURED),rain_d=n_distinct(DATE),inj_rat=injured/rain_d/incidents,
-            ped_inj_rat=ped_injured/rain_d/incidents,taxi=sum(VEHICLE.TYPE.CODE.1=="TAXI"),
-            twowheels=sum(VEHICLE.TYPE.CODE.1 %in% c("BICYCLE","MOTORCYCLE","SCOOTER")),
-                          pas_vehicle=sum(VEHICLE.TYPE.CODE.1=="PASSENGER VEHICLE"))
-by_rain
+by_rain<-group_by(M,rainy_days) %>% summarize(incidents=n(),rain_d=n_distinct(DATE),ped_inj==sum(VEHICLE.TYPE.CODE.1=="TAXI")/rain_d
+            taxi=sum(VEHICLE.TYPE.CODE.1=="TAXI")/rain_d,
+            twowheels=sum(VEHICLE.TYPE.CODE.1 %in% c("BICYCLE","MOTORCYCLE","SCOOTER"))/rain_d,
+            pas_vehicle=sum(VEHICLE.TYPE.CODE.1=="PASSENGER VEHICLE")/rain_d,
+            bus=sum(VEHICLE.TYPE.CODE.1=="BUS")/rain_d)
+
 x<-as.data.frame(by_rain)
+x<-select(x,taxi:bus)
+x<-apply(x,2,function(x) x/sum(x))
+x<-melt(x)
+x
+x$rain<-c(rep(c(0,1),4))
+x
+
+
+xx<-melt(x)
+xx
+melt(x)
+head(xx)
+xx<-x[,8:10]
+
+ggplot(x,aes(X2,value,fill=as.factor(rain))) + geom_bar(stat="identity") 
+
+xx$rain<-c(0,1,0,1,0,1)
+
+qplot(x)
+qplot()
+melt(x)
+
+require(reshape)
 x$per<-x$ped_inj_rat/sum(x$ped_inj_rat)
 x
 pie (x$per,labels = c(rain))
