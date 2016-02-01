124c7d3795c1aa7982ce6e6da6313f9ebde1c16e
udicuty <udi.rubin@gmail.com>
2016-01-31 20:43:45 -0500

    Project suggestion second ver - added histograms

--- TDIq3.R
+++ TDIq3.R
@@ -4,72 +4,90 @@ require(lubridate)
 require(ggplot2)
 require(gtools)
 
-NYPD<-fread("Data/NYPD_Motor_Vehicle_Collisions.csv",data.table=FALSE)
-colnames(NYPD)<-make.names(colnames(NYPD))
-#colnames(NYPD)
-#dim(NYPD)
 
-NYPD$DATE<-mdy(NYPD$DATE)
-
-M<-filter(NYPD,BOROUGH=="MANHATTAN") %>% arrange(DATE)
-#M<-filter(NYPD) %>% arrange(DATE)
-
-#M<-filter(NYPD) %>% arrange(DATE)
-#M1<-select(M,DATE,TIME,BOROUGH,NUMBER.OF.PEDESTRIANS.INJURED)
-
-i<-group_by(M,DATE) %>% summarize(count=n(),injured=sum(NUMBER.OF.PEDESTRIANS.INJURED),death=sum(NUMBER.OF.PERSONS.KILLED)) #%>% filter(injured<80)
-
-
+#Reading central park temperatures and filtering for dates that intersect with NYPD accident records
 w<-fread("Data/central_park_temp.csv",data.table=FALSE)
 w$DATE<-ymd(w$DATE)
-w<-filter(w,DATE>="2012-6-30" & DATE<="2016-01-26",PRCP<=2000)
+w<-filter(w,PRCP<1000) # Removing outliers
 
+#Reading NYPD data for Manhattan only
+NYPD<-fread("Data/NYPD_Motor_Vehicle_Collisions.csv",data.table=FALSE)
+colnames(NYPD)<-make.names(colnames(NYPD))
+NYPD$DATE<-mdy(NYPD$DATE)
+M<-filter(NYPD,BOROUGH=="MANHATTAN") %>% arrange(DATE)
+#M<-filter(NYPD,NUMBER.OF.PERSONS.INJURED<100) %>% arrange(DATE) #Removing outliers
+#M$MONTH<-substring(M$DATE,1,7)
+
+#Summarizing information from NYPD file, "rat" stands for injured/incidents ratio
+i<-group_by(M,DATE) %>% summarize(incidents=n(),injured=sum(NUMBER.OF.PERSONS.INJURED),ped_injured=sum(NUMBER.OF.PEDESTRIANS.INJURED),death=sum(NUMBER.OF.PERSONS.KILLED),rat=injured/incidents)
+
+
+#Considering only dates to which we have information in both databases
 common_dates<-intersect(w$DATE,i$DATE)
 w<-filter(w,DATE %in% common_dates)
 i<-filter(i,DATE %in% common_dates)
+M<-filter(M,DATE %in% common_dates)
 
 
-i$q<-quantcut(i$injured, q=seq(0,1,by=0.25), na.rm=TRUE,c(1:4))
-
-
-
-#hist(i$injured)
-
-#w$PRCP<-w$PRCP
-#plot(w$PRCP,i$injured)
-#cor(w$PRCP,i$injured)
-
-#abline(lm(i$injured~w$PRCP),col="red")
-
+#Merging NYPD with weather data
 d<-cbind(i,PRCP=w$PRCP)
-d$death<-d$death>0
-#group_by(d,q) %>% summarize(intercept=as.numeric(coef(lm(injured~PRCP))[1]),slope=as.numeric(coef(lm(injured~PRCP))[1]))
 
+#Calculating corellation and fitting linear model between injuries and percipitation levels
+cor(d$injured,d$PRCP)
+x<-coef(lm(d$injured~d$PRCP))
+xintercept<-as.numeric(x[1])
+xslope<-as.numeric(x[2])
+xintercept
+xslope
 
-q1<-filter(d,q==1)
-q1l<-coef(lm(q1$injured~q1$PRCP))
-q1l
-
-q2<-filter(d,q==2)
-q2l<-coef(lm(q2$injured~q2$PRCP))
-q2l
-
-q3<-filter(d,q==3)
-q3l<-coef(lm(q3$injured~q3$PRCP))
-q3l
-
-
-q4<-filter(d,q==4)
-q4l<-coef(lm(q4$injured~q4$PRCP))
-q4l
+cor(d$ped_injured,d$PRCP)
+y<-coef(lm(d$ped_injured~d$PRCP))
+yintercept<-as.numeric(y[1])
+yslope<-as.numeric(y[2])
+yslope
+#d$cyclist<-as.factor(ifelse(d$cyclist>0,1,0))
+ggplot(d,aes(PRCP,ped_injured)) + geom_point() + geom_abline(intercept= xintercept,slope=xslope,col="Red") + geom_abline(intercept= yintercept,slope=yslope,col="Red")
 
 
 
-injuredcoef<-lm(d$injured~d$PRCP)
 
-ggplot(d,aes(PRCP,injured)) + geom_point(aes(col=death)) + geom_abline(intercept=6.76,slope=0.009)
 
-#w$PRCP<-w$PRCP*100
-#plot(w$PRCP,i$count)
-#cor(w$PRCP,i$count)
-#abline(lm(i$count~w$PRCP),col="red")
+
+#Setting categorical value for rainy days
+rainy_days<-w$DATE[w$PRCP>0]
+M$rainy_days<-M$DATE %in% rainy_days
+
+
+x<-select(M,VEHICLE.TYPE.CODE.1,NUMBER.OF.PERSONS.INJURED,NUMBER.OF.PEDESTRIANS.INJURED,BOROUGH,NUMBER.OF.PERSONS.KILLED,rainy_days)
+x[x==""]<-"UNKNOWN"
+
+
+
+by_rain<-group_by(M,rainy_days) %>% summarize(incidents=n(),injured=sum(NUMBER.OF.PERSONS.INJURED),
+            ped_injured=sum(NUMBER.OF.PEDESTRIANS.INJURED),rain_d=n_distinct(DATE),inj_rat=injured/rain_d/incidents,
+            ped_inj_rat=ped_injured/rain_d/incidents,taxi=sum(VEHICLE.TYPE.CODE.1=="TAXI"),
+            twowheels=sum(VEHICLE.TYPE.CODE.1 %in% c("BICYCLE","MOTORCYCLE","SCOOTER")),
+                          pas_vehicle=sum(VEHICLE.TYPE.CODE.1=="PASSENGER VEHICLE"))
+by_rain
+x<-as.data.frame(by_rain)
+x$per<-x$ped_inj_rat/sum(x$ped_inj_rat)
+x
+pie (x$per,labels = c(rain))
+
+ggplot()
+
+ggplot(by_rain)
+
+
+
+#i$q<-quantcut(i$injured, q=seq(0,1,by=0.25), na.rm=TRUE,c(1:4))
+
+
+
+
+
+#ggplot(x,aes(x==BOROUGH)) + geom_histogram()
+#ggplot(x,aes(x=count,y=MONTH)) + geom_bar()
+#qplot(factor(MONTH),datax$count,geom_bar())
+
+#qplot(x$count,fill=x$VEHICLE.TYPE.CODE.1)
